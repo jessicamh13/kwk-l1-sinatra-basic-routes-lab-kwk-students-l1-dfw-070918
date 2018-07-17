9cd51c340359cb75216aa5135b0c39d6b733ccd3
jessicamh13 <jessica.mh13@yahoo.com>
2018-07-17 16:45:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,16 @@ get '/' do
   "Hello, World!"
 end
 
+get '/name'
+  "My name is Jessica"
+end
+
+get '/hometown'
+  "My hometown is Miami, Florida"
+end 
+
+get '/favorite-song'
+  "My favorite song is Safe and Sound by Taylor Swift"
+end 
+
 end
